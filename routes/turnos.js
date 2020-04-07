e00349721fa45568f9f1a322ec79a511402e2f21
dpresmanes <damianpresmanes@gmail.com>
2020-04-07 10:33:05 -0300

    first commit

---
+++ routes/turnos.js
@@ -0,0 +1,22 @@
+var express = require ('express');
+var router = express.Router();
+
+
+router.get('/', (req,res,next)=> {
+    res.render('turno');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
